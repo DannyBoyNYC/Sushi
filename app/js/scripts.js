278346ed1e4fb10ae78440fbbc58af2f1a94aa81
DannyBoyNYC <daniel.deverell@gmail.com>
2025-02-17 11:40:49 -0500

    done

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,11 +1,13 @@
 var body = document.querySelector("body");
-document.addEventListener("click", show);
+document.addEventListener("click", handleClicks);
 
-function show(e) {
+function handleClicks(e) {
   if (e.target.matches(".map")) {
-    e.preventDefault();
     body.classList.add("showme");
-  } else {
+    e.preventDefault();
+  }
+  if (e.target.matches(".closer")) {
     body.classList.remove("showme");
+    e.preventDefault();
   }
 }
